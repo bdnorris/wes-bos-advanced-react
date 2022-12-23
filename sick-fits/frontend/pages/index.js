092a50d1545202d89b1ade45aa1f584ea01e6b07
Brian <bdnorris@gmail.com>
2022-12-23 10:00:21 -0600

    refactoring to use _app.js

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,10 +1,8 @@
-import Page from '../components/Page';
-
 export default function IndexPage() {
   return (
-    <Page cool="testing">
+    <>
       <h1>My page</h1>
       <p>Some content</p>
-    </Page>
+    </>
   );
 }
