642688c931399c825ad3267de23033646dbad908
Brian <bdnorris@gmail.com>
2022-12-23 10:24:32 -0600

    adding header and nav components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,8 +1,10 @@
 import propTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header />
       <p>I am the Page Component!</p>
       {children}
       <h2>{cool}</h2>
