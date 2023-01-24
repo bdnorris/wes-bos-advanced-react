955a2af8cacfee6ef43591ad9a1cc8b10761bed9
Brian <bdnorris@gmail.com>
2023-01-23 19:49:48 -0600

    single product finish

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,34 +1,8 @@
-import propTypes from 'prop-types';
-
 import { useRouter } from 'next/router';
-import gql from 'graphql-tag';
-import { useQuery } from '@apollo/client';
+import SingleProduct from '../../components/SingleProduct';
 
-const SINGLE_PRODUCT_QUERY = gql`
-  query SINGLE_PRODUCT_QUERY($id: ID!) {
-    Product(where: { id: $id }) {
-      id
-      name
-      description
-      price
-    }
-  }
-`;
-
-export default function SingleProduct() {
+export default function SingleProductPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
-    variables: { id },
-  });
-  console.log({ data, error, loading });
-  return (
-    <div>
-      <p>Single Product {id}</p>
-    </div>
-  );
+  return <SingleProduct id={id} />;
 }
-
-SingleProduct.propTypes = {
-  // query: propTypes.object,
-};
